6a94f64ee2b80d321f2f0d543eb9fdfcd8daf6bd
Volodymyr_Tryhub <tryhub9@gmail.com>
2026-06-03 23:09:01 +0300

    fix metadata and note form

--- app/notes/action/create/page.tsx
+++ app/notes/action/create/page.tsx
@@ -10,8 +10,12 @@ export const metadata: Metadata = {
   openGraph: {
     title: 'Create Note | NoteHub',
     description: 'Create a new note in NoteHub',
-    url: 'https://06-notehub-nextjs.vercel.app',
-    images: ['https://ac.goit.global/fullstack/react/notehub-og-meta.jpg'],
+    url: 'https://08-zustand-8jnv.vercel.app/notes/action/create',
+    images: [
+      {
+        url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+      },
+    ],
   },
 };
 
